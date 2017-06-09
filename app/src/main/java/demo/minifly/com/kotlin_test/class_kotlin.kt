6da6ef81ddc09dc3afed916631a9835a7ca4f44d
贺晓飞 <hexiaofei@iscs.com.cn>
2017-06-09 17:39:07 +0800

    kotlin实验.

---
+++ app/src/main/java/demo/minifly/com/kotlin_test/class_kotlin.kt
@@ -0,0 +1,15 @@
+package demo.minifly.com.kotlin_test
+
+/**
+ * author ：minifly
+ * date: 2017/6/9
+ * time: 16:43
+ * desc: kotlin的类
+ */
+
+class Person{
+    var name: String = ""
+    var age:Int =0
+    var college: String?=null
+}
+
